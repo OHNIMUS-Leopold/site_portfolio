d678b40ac1e548f18b8528e5cffe23573d5099ae
OHNIMUS-Leopold <leopold.ohnimus@edu.univ-fcomte.fr>
2023-03-16 20:09:43 +0100

    paillettes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,57 +1,167 @@
 <template>
   <div class="mt-8">
-    <h1 class="font-Poppins text-black uppercase text-center leading-tight text-5xl font-bold">Léopold Ohnimus</h1>
-    <img class="w-60 mx-auto mt-4" src="/pp.webp" alt="Léopold Ohnimus">
+    <h1 class="font-Poppins text-black uppercase text-center leading-tight text-5xl font-bold zooom">Léopold Ohnimus</h1>
+    <img class="w-60 mx-auto mt-4 rounded-full animate-spin hover:opacity-10 transition-opacity" src="/pp.webp" alt="Léopold Ohnimus">
   </div>
   <div class="mt-8 mb-10 flex flex-col">
     <h2 class="font-Poppins font-bold text-center text-black text-2xl">Mes réseaux</h2>
     <div class="flex mx-auto space-x-6 mt-4">
-      <a href="https://www.linkedin.com/in/leopold-ohnimus/"><img src="../assets/img/linkedin.png" alt="linkedin" class="h-11"></a>
-      <a href="https://github.com/OHNIMUS-Leopold"><img src="../assets/img/github.png" alt="github" class="h-11"></a>
-      <a href="https://www.instagram.com/opop_stan_loona/"><img src="../assets/img/instagram.png" alt="instagram" class="h-11"></a>
-      <a href="https://twitter.com/OpopStanLOONA"><img src="../assets/img/twitter.png" alt="twitter" class="h-11"></a>
-      <a href="https://open.spotify.com/user/mke3r6jcqoz4rpt1jughoh6ax"><img src="../assets/img/spotify.png" alt="spotify" class="h-11"></a>
+      <a href="https://www.linkedin.com/in/leopold-ohnimus/"><img src="../assets/img/linkedin.png" alt="linkedin" class="h-11 icn"></a>
+      <a href="https://github.com/OHNIMUS-Leopold"><img src="../assets/img/github.png" alt="github" class="h-11 icn"></a>
+      <a href="https://www.instagram.com/opop_stan_loona/"><img src="../assets/img/instagram.png" alt="instagram" class="h-11 icn"></a>
+      <a href="https://twitter.com/OpopStanLOONA"><img src="../assets/img/twitter.png" alt="twitter" class="h-11 icn"></a>
+      <a href="https://open.spotify.com/user/mke3r6jcqoz4rpt1jughoh6ax"><img src="../assets/img/spotify.png" alt="spotify" class="h-11 icn"></a>
     </div>
   </div>
   <div class="mb-4">
     <h2 class="font-Poppins font-bold text-center text-black text-2xl">Mes projets universitaires</h2>
       <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(300px,1fr))] justify-items-center">
-        <div class="mt-6 border-black border-2 w-[280px] p-2 flex flex-col ">
+        <div class="mt-6 border-black border-2 w-[280px] p-2 flex flex-col woo">
           <div class="mx-auto"><h3 class="font-Poppins font-normal text-black text-xl">Korean Notes</h3></div>
           <div class="flex mx-auto space-x-4 my-3">
             <!-- <a href="https://www.figma.com/file/bMuNkyo4yT6sfkvQGzqfrE/R209---L%C3%A9opold-OHNIMUS?node-id=0%3A1"><img src="../assets/img/figma.png" alt="identité visuelle" class="h-9"></a> -->
-            <a href="https://www.figma.com/file/9cyqmICslBXaKZIcQ4vZRq/Maquettes-projet-individuel-%E2%80%94-L%C3%A9opold-OHNIMUS?node-id=0%3A1&t=ccPBTveqxUVCZidj-1"><img src="../assets/img/figma.png" alt="maquette" class="h-9"></a>
-            <a href="https://github.com/MMI-SAE-105/2022-sae105-projet-OHNIMUS-Leopold"><img src="../assets/img/github.png" alt="code source" class="h-9"></a>
+            <a href="https://www.figma.com/file/9cyqmICslBXaKZIcQ4vZRq/Maquettes-projet-individuel-%E2%80%94-L%C3%A9opold-OHNIMUS?node-id=0%3A1&t=ccPBTveqxUVCZidj-1"><img src="../assets/img/figma.png" alt="maquette" class="h-9 icn"></a>
+            <a href="https://github.com/MMI-SAE-105/2022-sae105-projet-OHNIMUS-Leopold"><img src="../assets/img/github.png" alt="code source" class="h-9 icn"></a>
           </div>
           <div class="mx-auto"><a href="https://korean-notes.leopold-ohnimus.fr/"><h4 class="font-Poppins font-normal text-center text-black text-xs">https://korean-notes.leopold-ohnimus.fr/</h4></a></div>
         </div>
-        <div class="mt-6 border-black border-2 w-[280px] p-2 flex flex-col ">
+        <div class="mt-6 border-black border-2 w-[280px] p-2 flex flex-col waa">
           <div class="mx-auto"><h3 class="font-Poppins font-normal text-black text-xl">POP KONNECT 서울</h3></div>
           <div class="flex mx-auto space-x-4 my-3">
-            <a href="https://www.figma.com/file/bMuNkyo4yT6sfkvQGzqfrE/R209---L%C3%A9opold-OHNIMUS?node-id=0%3A1"><img src="../assets/img/figma.png" alt="identité visuelle" class="h-9"></a>
-            <a href="https://www.figma.com/file/p36UwfbiOgsrn7BNV9gT37/R209---Maquettes---L%C3%A9opold-OHNIMUS"><img src="../assets/img/figma.png" alt="maquette" class="h-9"></a>
-            <a href="https://github.com/OHNIMUS-Leopold/SAE_DEV"><img src="../assets/img/github.png" alt="code source" class="h-9"></a>
+            <a href="https://www.figma.com/file/bMuNkyo4yT6sfkvQGzqfrE/R209---L%C3%A9opold-OHNIMUS?node-id=0%3A1"><img src="../assets/img/figma.png" alt="identité visuelle" class="h-9 icn"></a>
+            <a href="https://www.figma.com/file/p36UwfbiOgsrn7BNV9gT37/R209---Maquettes---L%C3%A9opold-OHNIMUS"><img src="../assets/img/figma.png" alt="maquette" class="h-9 icn"></a>
+            <a href="https://github.com/OHNIMUS-Leopold/SAE_DEV"><img src="../assets/img/github.png" alt="code source" class="h-9 icn"></a>
           </div>
           <div class="mx-auto"><a href="https://pop-konnect.leopold-ohnimus.fr/"><h4 class="font-Poppins font-normal text-center text-black text-xs">https://pop-konnect.leopold-ohnimus.fr/</h4></a></div>
         </div>
-        <div class="mt-6 border-black border-2 w-[280px] p-2 flex flex-col ">
+        <div class="mt-6 border-black border-2 w-[280px] p-2 flex flex-col wii">
           <div class="mx-auto"><h3 class="font-Poppins font-normal text-black text-xl">Citadelle de Besançon</h3></div>
           <div class="flex mx-auto space-x-4 my-3">
             <!-- <a href="https://www.figma.com/file/bMuNkyo4yT6sfkvQGzqfrE/R209---L%C3%A9opold-OHNIMUS?node-id=0%3A1"><img src="../assets/img/figma.png" alt="identité visuelle" class="h-9"></a>
             <a href="https://www.figma.com/file/p36UwfbiOgsrn7BNV9gT37/R209---Maquettes---L%C3%A9opold-OHNIMUS"><img src="../assets/img/figma.png" alt="maquette" class="h-9"></a> -->
-            <a href="https://github.com/OHNIMUS-Leopold/code_citadelle"><img src="../assets/img/github.png" alt="code source" class="h-9"></a>
+            <a href="https://github.com/OHNIMUS-Leopold/code_citadelle"><img src="../assets/img/github.png" alt="code source" class="h-9 icn"></a>
           </div>
           <div class="mx-auto"><a href="https://citadelle-besancon.leopold-ohnimus.fr/"><h4 class="font-Poppins font-normal text-center text-black text-xs">https://citadelle-besancon.leopold-ohnimus.fr/</h4></a></div>
         </div>
-        <div class="mt-6 border-black border-2 w-[280px] p-2 flex flex-col ">
+        <div class="mt-6 border-black border-2 w-[280px] p-2 flex flex-col woo">
           <div class="mx-auto"><h3 class="font-Poppins font-normal text-black text-xl">TP Maquette WP</h3></div>
           <div class="flex mx-auto space-x-4 my-3">
             <!-- <a href="https://www.figma.com/file/bMuNkyo4yT6sfkvQGzqfrE/R209---L%C3%A9opold-OHNIMUS?node-id=0%3A1"><img src="../assets/img/figma.png" alt="identité visuelle" class="h-9"></a>
             <a href="https://www.figma.com/file/p36UwfbiOgsrn7BNV9gT37/R209---Maquettes---L%C3%A9opold-OHNIMUS"><img src="../assets/img/figma.png" alt="maquette" class="h-9"></a> -->
-            <a href="https://tp-wordpress.leopold-ohnimus.fr/"><img src="../assets/img/wordpress.png" alt="site" class="h-9"></a>
+            <a href="https://tp-wordpress.leopold-ohnimus.fr/"><img src="../assets/img/wordpress.png" alt="site" class="h-9 icn"></a>
           </div>
           <div class="mx-auto"><a href="https://tp-wordpress.leopold-ohnimus.fr/"><h4 class="font-Poppins font-normal text-center text-black text-xs">https://tp-wordpress.leopold-ohnimus.fr/</h4></a></div>
         </div>
       </div>
   </div>
 </template>
+
+
+<style>
+
+body {
+  /* background-color: #FFFFFF; */
+  animation: blink 1s infinite;
+}
+
+@keyframes blink {
+  0% {
+    background-color: red;
+  }
+  16.7% {
+    background-color: rgb(0, 255, 0);
+  }
+  33.4% {
+    background-color: yellow;
+  }
+  50.1% {
+    background-color: cyan;
+  }
+  66.8% {
+    background-color: orange;
+  }
+  83.5% {
+    background-color: magenta;
+  }
+  100% {
+    background-color: red;
+  }
+}
+
+
+.icn {
+  position: relative;
+  transition: transform 0.5s;
+}
+
+.icn:hover {
+  transform: translateY(-10px);
+}
+
+
+.zooom {
+    /* animation: zooom 1s cubic-bezier(0, 0, 0.2, 1) infinite; */
+    animation: zooom 0.2s infinite;
+  }
+
+@keyframes zooom {
+     100% {
+        opacity: 0;
+    }
+}
+
+
+
+
+@media (min-width: 1200px) {
+  .woo {
+    animation: wooo 1.6s infinite;
+}
+
+@keyframes wooo {
+    0%, 100% {
+        transform: translateY(-200%);
+        animation-timing-function: cubic-bezier(0.8,0,1,1);
+    }
+    50% {
+        transform: none;
+        animation-timing-function: cubic-bezier(0,0,0.2,1);
+    }
+}
+
+
+.waa {
+    animation: waaa 1s infinite;
+}
+
+@keyframes waaa {
+    0%, 100% {
+        transform: translateX(-25%);
+        animation-timing-function: cubic-bezier(0.8,0,1,1);
+    }
+    50% {
+        transform: none;
+        animation-timing-function: cubic-bezier(0,0,0.2,1);
+    }
+}
+
+
+.wii {
+    animation: wiii 1s infinite;
+}
+
+@keyframes wiii {
+    0%, 100% {
+        transform: translateX(25%);
+        animation-timing-function: cubic-bezier(0.8,0,1,1);
+    }
+    50% {
+        transform: none;
+        animation-timing-function: cubic-bezier(0,0,0.2,1);
+    }
+}
+}
+
+
+
+</style>
